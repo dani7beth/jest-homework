44faf1ba377e959e051706a3babbc21c1d57be16
dani7beth <danielledona28@gmail.com>
2020-12-02 14:36:24 -0700

    halfway mark through tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,18 +2,39 @@ import {
   // methods,
   // methods,
   originalArray,
-  doubleArray
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero
 } from '../app'
 
 // hint of look in before_each before_all
 describe('originalArray Test', ()=> {
   test('originalArray should return it self', ()=>{
     expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
-  })
-})
+  });
+});
 
 describe('doubleArray Test', ()=> {
-  test('originalArray should return it self', ()=>{
+  test('doubleArray should return values doubled', ()=>{
     expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8]);
-  })
-})
+  });
+});
+
+describe('evensOnlyArray Test', ()=> {
+  test('evensOnlyArray should return only even values', ()=>{
+    expect(evensOnlyArray([1,2,3,4])).toEqual([2,4]);
+  });
+});
+
+describe('arraySum Test', ()=> {
+  test('arraySum should return all values added together', ()=>{
+    expect(arraySum([1,2,3,4])).toEqual(10);
+  });
+});
+
+describe('allNumbersGreaterThanZero Test', ()=> {
+  test('allNumbersGreaterThanZero should return true : false if all elements are greater than zero', ()=>{
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toEqual(true);
+  });
+});
--- app.js
+++ app.js
@@ -2,6 +2,10 @@ export const originalArray = (arr) =>{
   return arr
 }
 
+// ARRAY ITERATOR METHODS
+// numberArray = [1,2,3,4]
+
+
 //return array with doulbe values in array
 //numberArray = [1,2,3,4] => [2,4,6,8]
 export const doubleArray = (numberArray) => {
@@ -9,3 +13,34 @@ export const doubleArray = (numberArray) => {
    return num * 2;
   })
 };
+
+
+// return array with all even numbers
+// evensOnlyArray(numberArray) // => [2,4];  hint use filter 
+export const evensOnlyArray = (numberArray) => {
+  return numberArray.filter((number) => number%2 === 0);
+};
+
+// return array with sum of numbers
+// arraySum(numberArray) // => 10;  hint use reduce
+export const arraySum = (numberArray) => {
+  return numberArray.reduce((sum, number) => sum + number);
+};
+
+// return true if every item greater than 0
+// allNumbersGreaterThanZero(numberArray) // => true; use every
+export const allNumbersGreaterThanZero = (numberArray) => { 
+   return numberArray.every(number =>  number > 0);
+};
+
+//  return true if one or more items in array are odd
+// someNumbersAreOdd(numberArray) // => true; use some
+
+//  return array double and even
+// evensOnlyAndDoubleArray(numberArray) // => [4,8]; // use map and filter
+
+//  find an item method return 'not found' if not found
+// findItem(numberArray, 1) //=> 1
+
+// sort array desc order
+// sortArray(numberArray) //=> [4,3,2,1] *desc*
