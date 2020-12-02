157ef707353d3c19982a9edc0b79dd5d9bd1c19c
dani7beth <danielledona28@gmail.com>
2020-12-02 13:55:49 -0700

    init test

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 export const originalArray = (arr) =>{
   return arr
 }
+
+//return array with doulbe values in array
+//numberArray = [1,2,3,4] => [2,4,6,8]
+export const doubleArray = (numberArray) => {
+  return numberArray.map(num =>{
+   return num * 2;
+  })
+};
